005dc8f051028438469e9b5e9d7d7daa4aec7f13
maureenyeung <57898367+maureenyeung@users.noreply.github.com>
2022-03-07 14:30:04 +0100

    Knikkerbaan Af

---
+++ arduino/knikkerbaan/knikkerbaanInleveren/_main.ino
@@ -0,0 +1,115 @@
+#include <Arduino_JSON.h>
+
+KnikkerPoort poortBoven = KnikkerPoort();
+WiFiCommunicator wifi = WiFiCommunicator(WIFI_NETWERK, WIFI_WACHTWOORD, SERVER_DOMEINNAAM);
+Teller tellerA = Teller(TELLER_A_PIN); // voor teller A
+Teller tellerB = Teller(TELLER_B_PIN); // voor teller B
+Teller tellerC = Teller(TELLER_C_PIN); // voor teller C
+
+int serverContactInterval = 3;                // na 3 seconden beweegt de servo weer
+unsigned long tijdVoorContactMetServer = 0;
+unsigned long tijdOmBuzzerUitTeZetten = 0;
+int vorigeAantalknikkers = 0;
+int vorigeAantalknikkersB = 0;
+int vorigeAantalknikkersC = 0;
+int toonBuzzer = 500; // standaart een toonhoogte op 1000, zodat als contact niet goed toch een toon
+int toonOntvangen;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUZZERPIN, OUTPUT);
+//  pinMode(BUZZERPINB, OUTPUT);
+  poortBoven.begin(BOVEN_POORT_PIN, 90, 10);
+
+  wifi.begin();
+
+  wifi.stuurVerzoek("/api/set/nieuwerun", "");
+
+  poortBoven.open();
+}
+
+
+void loop() {
+// laat de teller A detecteren:
+  tellerA.update();
+  if (vorigeAantalknikkers < tellerA.getAantal()) {
+    // digitalWrite(BUZZERPIN, HIGH);
+    tone(BUZZERPIN, toonBuzzer, 250);
+    Serial.println("zet de buzzer aan");
+    
+//laat de teller B detecteren:
+    tellerB.update();
+    if (vorigeAantalknikkersB < tellerB.getAantal()) {
+      // digitalWrite(BUZZERPIN, HIGH);
+      tone(BUZZERPIN, toonBuzzer, 250);
+      Serial.println("zet de buzzer aan");
+
+    }
+
+    tellerC.update();
+    if (vorigeAantalknikkersC < tellerC.getAantal()) {
+      // digitalWrite(BUZZERPIN, HIGH);
+      tone(BUZZERPIN, toonBuzzer, 250);
+      Serial.println("zet de buzzer aan");
+
+    }
+    vorigeAantalknikkers = tellerA.getAantal();
+    vorigeAantalknikkersB = tellerB.getAantal();
+    vorigeAantalknikkersC = tellerC.getAantal();
+
+
+    // pauzeer de knikkerbaan als het tijd is voor contact met server
+    if (millis() > tijdVoorContactMetServer && poortBoven.getOpen()) {
+      Serial.println("de poort sluit");
+      poortBoven.sluit();
+    }
+
+    // knikkerbaan is leeggelopen, er zijn geen sensors dit iets moeten meten
+    // nu is het tijd om contact te leggen met de server:
+    if (millis() > tijdVoorContactMetServer + LEEGLOOP_TIJD) {
+      Serial.println("Nu zouden we met de server moeten praten");
+      Serial.print("Er zijn nu zoveel knikkers geteld: ");
+      Serial.println(tellerA.getAantal());
+      Serial.println(tellerB.getAantal());
+      Serial.println(tellerC.getAantal());
+
+      // intellingen ophalen door Arduino > filmpje
+      String data = "knikkers=";
+      data += tellerA.getAantal();
+
+      wifi.stuurVerzoek("/api/set/sensordata", data.c_str());
+
+      String serverAntwoord = wifi.stuurVerzoek("/api/get/instellingen", "");
+      Serial.println(serverAntwoord);
+
+      JSONVar ontvangenInstellingen = JSON.parse(serverAntwoord);
+
+      if (JSON.typeof(ontvangenInstellingen != "undefined")){
+        toonOntvangen = (int)ontvangenInstellingen["toonhoogte"];
+      }
+      else {
+        Serial.println("FOUT: geen serverAntwoord");
+      }
+
+       // server communucatie afgerond
+
+      // zorgen voor verandering in toonhoogte bij ontvangen info
+      if (toonOntvangen = "hoog"){
+        toonBuzzer = 1000; 
+      }
+      else{
+        toonBuzzer = 300;
+      }
+
+
+
+      // bereken de nieuwe tijd waarop er weer met de server gecommuniceerd moet worden
+      tijdVoorContactMetServer = millis() + (unsigned long)serverContactInterval * 1000;
+      // en zet nu het poortje weer open:
+      poortBoven.open();
+      Serial.println("de poort gaat weer open");
+
+    }
+  }  
+  
+}
---
+++ arduino/knikkerbaan/knikkerbaanInleveren/knikkerbaanInleveren.ino
@@ -0,0 +1,26 @@
+
+
+//declareer hier je globale constanten:
+
+const int BOVEN_POORT_PIN = 6;    // pin van servo die bovenste poort regelt
+const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
+                                  // sluiten van de poort langs alle sensors te komen
+
+char WIFI_NETWERK[] = "informatica-fast-wifi";
+char WIFI_WACHTWOORD[] = "informatica-rocks!";
+char SERVER_DOMEINNAAM[] = "salmon-centipede-vop8j9ds.ws-eu21.gitpod.io";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
+
+const int TELLER_A_PIN = 5;       // pin waarop IR-sensor voor Teller A is aangesloten
+const int TELLER_B_PIN = 10;       // pin waarop IR-sensor voor Teller B is aangesloten //voor teller B
+const int TELLER_C_PIN = 11;
+
+const int BUZZERPIN = 9; //buzzer to arduino pin 9 voor teller A en B 
+
+//
+//
+//void setup(){
+//
+//  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+//
+//}
+//
